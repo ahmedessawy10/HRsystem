1e9c924cddf7505edaa8d071b2532fd8f867dd35
ahmedessawy10 <elessawy238@gmail.com>
2025-03-21 12:53:58 +0200

    continous edit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-            'changepassword' => CheckChangePassword::class, 
+            'changepassword' => CheckChangePassword::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
